6394dbe6b9ff1f35f6000fca50daf2af770432ee
DariaRodovnichenko <125218414+DariaRodovnichenko@users.noreply.github.com>
2025-04-09 11:08:31 +0200

    translation

---
+++ js/translations.js
@@ -0,0 +1,370 @@
+const translations = {
+  en: {
+    //   About Me
+    aboutMe: [
+      {
+        name: 'Daria Rodovnichenko',
+        profession: 'Junior Front-End Developer',
+        summary:
+          'Junior Front-End Developer with hands-on experience building responsive web applications and a solid grasp of modern technologies, tools, and development best practices. I am currently deepening my expertise through a JavaScript development program that includes personal projects. I am eager to bring a blend of technical and customer-focused skills to a dynamic team.',
+
+        skills: 'Projects',
+        list: [
+          {
+            title: 'Full Project Developer',
+            github: 'https://github.com/DariaRodovnichenko/ssd-ng_ts',
+            preview: 'https://dariarodovnichenko.github.io/ssd-ng_ts/',
+          },
+          {
+            title: 'Full Project Developer',
+            github: 'https://github.com/DariaRodovnichenko/crf-annex',
+            preview: 'https://dariarodovnichenko.github.io/crf-annex/',
+          },
+          {
+            title: 'Full Project Developer',
+            github:
+              'https://github.com/DariaRodovnichenko/coffee-recipe-finder',
+            preview:
+              'https://dariarodovnichenko.github.io/coffee-recipe-finder/',
+          },
+          {
+            title: 'Backend Developer',
+            github: 'https://avk9.github.io/water_reacT/welcome',
+            preview: 'https://avk9.github.io/water_reacT/welcome',
+          },
+          {
+            title: 'Full Project Developer',
+            github:
+              'https://dariarodovnichenko.github.io/goit-react-hw-05-movies/',
+            preview:
+              'https://dariarodovnichenko.github.io/goit-react-hw-05-movies/',
+          },
+          {
+            title: 'Modal Window Developer',
+            github: 'https://yurchykyur.github.io/project-stephen-js/',
+            preview: 'https://yurchykyur.github.io/project-stephen-js/',
+          },
+          {
+            title: 'Scrum Master / Header Developer',
+            github: 'https://kilsor.github.io/team-project/',
+            preview: 'https://kilsor.github.io/team-project/',
+          },
+        ],
+      },
+    ],
+
+    // Work Experience
+    workExperienceTitle: 'Work Experience',
+    workExperience: [
+      {
+        title: 'Barista',
+        company: 'MAME Romandie Gmbh | Geneva, Switzerland',
+        period: 'September 2023 - August 2024',
+        responsibilities: [
+          'Assist customers in selecting different coffees, enhancing their overall experience.',
+          'Managed daily operations, including cost tracking and ensuring customer service quality.',
+        ],
+      },
+      {
+        title: 'Co-Owner and Brand Barista',
+        company: 'Industrial Coffee | Kyiv, Ukraine',
+        period: 'July 2020 - May 2023',
+        responsibilities: [
+          'Manage all aspects of the facility, including schedule planning, stock management, and collaboration with suppliers.',
+          'Optimize processes to increase profitability by 10% in the first year.',
+          'Trained and integrated team members to maintain high service standards and performance.',
+          'Lead process optimization strategy, ensuring efficient workflow and continuous improvement of company profitability.',
+        ],
+      },
+      {
+        title: 'Barista',
+        company: 'Filomena SA | Geneva, Switzerland',
+        period: 'July 2022 - March 2023',
+        responsibilities: [
+          'Direct experience with customers, ensuring prompt and attentive service in compliance with quality standards',
+        ],
+      },
+      {
+        title: 'Sales Manager | Barista',
+        company: 'Dim Kavy | Kyiv, Ukraine',
+        period: 'April 2017 - December 2019',
+        responsibilities: [
+          'Ensuring daily team and operations management in a high-intensity environment.',
+          'Training staff in service and point-of-sale management skills.',
+        ],
+      },
+      {
+        title: 'Barman-Administrator',
+        company: 'Camden Coffee Shop| Kyiv, Ukraine',
+        period: 'September 2016 - April 2017',
+        responsibilities: [
+          'Managing all administrative tasks of daily operations, developing the ability to effectively manage multiple priorities.',
+        ],
+      },
+      {
+        title: 'Regional Marketing Manager',
+        company: 'S Group | Kyiv, Ukraine',
+        period: 'September 2015 - April 2016',
+        responsibilities: [
+          "Collect and analyze marketing data on the company's products and items.",
+          'Develop and maintain comparative sales tables by point-of-sale distribution.',
+        ],
+      },
+    ],
+    // Side Bar
+    contacts: {
+      title: 'Contacts',
+      phone: '+41 76 702 86 65',
+      email: 'daria.rodovnichenko@gmail.com',
+      linkedin: {
+        label: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/daria-rodovnichenko/',
+      },
+    },
+    educationTitle: 'Education',
+    education: [
+      {
+        school: 'Nomades Advanced Technologies | Geneva',
+        specialization: 'Angular Developer',
+        period: 'March 2025 - May 2025',
+      },
+      {
+        school: 'Nomades Advanced Technologies | Geneva',
+        specialization: 'JS Software Developer',
+        period: 'January 2025 - March 2025',
+      },
+      {
+        school: 'IT School GoIt | Ukraine',
+        specialization: 'Fullstack Developer',
+        period: 'February 2023 - April 2024',
+      },
+      {
+        university:
+          'Kyiv National University of Trade and Economics | Kyiv, Ukraine',
+        specialization: 'Master of Marketing',
+        period: 'September 2010 - September 2015',
+      },
+    ],
+    techSkills: [
+      {
+        title: 'Tech Skills',
+        skills: [
+          'HTML',
+          'CSS',
+          'JavaScript',
+          'React',
+          'TypeScript',
+          'Angular',
+          'Firebase',
+          'GIT',
+          'Slack',
+          'Trello',
+        ],
+      },
+    ],
+    softSkills: [
+      {
+        title: 'Soft Skills',
+        skills: [
+          'Rigor and attention to detail',
+          'Ability to organize and work independently',
+          'Excellent interpersonal skills and customer service',
+        ],
+      },
+    ],
+    languages: [
+      {
+        title: 'Languages',
+        list: ['English: C1', 'French: B2', 'Ukrainian: Native', 'Russian'],
+      },
+    ],
+  },
+
+  // French
+  fr: {
+    //   About Me
+    aboutMe: [
+      {
+        name: 'Daria Rodovnichenko',
+        profession: 'Développeur Front-End Junior',
+        summary:
+          "Développeur Front-End Junior avec une expérience pratique dans la création d'applications web réactives et une solide maîtrise des technologies modernes, des outils et des bonnes pratiques de développement. Actuellement, j'approfondis mon expertise à travers    un programme de développement JavaScript incluant le projet personnel. Désireuse d'apporter un mélange de compétences techniques et orientées client au sein d'une équipe dynamique.",
+
+        skills: 'Projets',
+        list: [
+          {
+            title: 'Développeur de projet complet',
+            github: 'https://github.com/DariaRodovnichenko/ssd-ng_ts',
+            preview: 'https://dariarodovnichenko.github.io/ssd-ng_ts/',
+          },
+          {
+            title: 'Développeur de projet complet',
+            github: 'https://github.com/DariaRodovnichenko/crf-annex',
+            preview: 'https://dariarodovnichenko.github.io/crf-annex/',
+          },
+          {
+            title: 'Développeur de projet complet',
+            github:
+              'https://github.com/DariaRodovnichenko/coffee-recipe-finder',
+            preview:
+              'https://dariarodovnichenko.github.io/coffee-recipe-finder/',
+          },
+          {
+            title: 'Backend Developer',
+            github: 'https://avk9.github.io/water_reacT/welcome',
+            preview: 'https://avk9.github.io/water_reacT/welcome',
+          },
+          {
+            title: 'Développeur de projet complet',
+            github:
+              'https://dariarodovnichenko.github.io/goit-react-hw-05-movies/',
+            preview:
+              'https://dariarodovnichenko.github.io/goit-react-hw-05-movies/',
+          },
+          {
+            title: 'Développeur modal',
+            github: 'https://yurchykyur.github.io/project-stephen-js/',
+            preview: 'https://yurchykyur.github.io/project-stephen-js/',
+          },
+          {
+            title: 'Scrum master / Développeur header',
+            github: 'https://kilsor.github.io/team-project/',
+            preview: 'https://kilsor.github.io/team-project/',
+          },
+        ],
+      },
+    ],
+
+    // Work Experience
+    workExperienceTitle: 'Expérience Professionnelle',
+    workExperience: [
+      {
+        title: 'Barista',
+        company: 'MAME Romandie Gmbh | Genève, Suisse',
+        period: 'Septembre 2023 - Août 2024',
+        responsibilities: [
+          'Assistance aux clients dans la selection des différents cafés, contribuant à améliorer leur experience globale.',
+          'Gestion des opérations quotidiennes, incluant le suivi des coûts et le contrôle de qualité du service client',
+        ],
+      },
+      {
+        title: 'Propriétaire et chef-barista',
+        company: 'Industrial Coffee | Kyiv, Ukraine',
+        period: 'Juillet 2020 - Mai 2023',
+        responsibilities: [
+          "Gestion complète de l'établissement, y compris la planification des horaires, la gestion des stocks et la collaboration avec les fournisseurs",
+          'Optimisation des processus pour accroître la rentabilité de 5% la première année',
+          "Formation et intégration des membres de l'équipe pour maintenir des standards élevés deservice et de performance.",
+          "Responsabilité directe de la stratégie d'optimisation des processus, garantissant un flux de travail efficace et une amélioration continue de la rentabilité de l'entreprise.",
+        ],
+      },
+      {
+        title: 'Barista',
+        company: 'Filomena SA | Genève, Suisse',
+        period: 'Juillet 2022 - Mars 2023',
+        responsibilities: [
+          'Expérience directe avec les clients, assurant un service rapide et attentionné, en conformité avec les standards de qualité.',
+        ],
+      },
+      {
+        title: 'Responsable des ventes | Barista',
+        company: 'Dim Kavy | Kyiv, Ukraine',
+        period: 'Avril 2017 - Decembre 2019',
+        responsibilities: [
+          "Assurance de la gestion quotidienne de l'équipe et des opérations dans un environnement à haute intensité.",
+          'Formation du personnel aux compétences de service et de gestion de point de vente.',
+        ],
+      },
+      {
+        title: 'Barman-Administrator',
+        company: 'Camden Coffee Shop| Kyiv, Ukraine',
+        period: 'Septembre 2016 - Avril 2017',
+        responsibilities: [
+          'Gestion de toutes les tâches administratives des opérations quotidiennes, développant la capacité de gérer efficacement plusieurs priorités.',
+        ],
+      },
+      {
+        title: 'Responsable Marketing Regional',
+        company: 'S Group | Kyiv, Ukraine',
+        period: 'Septembre 2015 - Avril 2016',
+        responsibilities: [
+          "Recueillir et analyser les données marketing sur les produits et articles de l'entreprise.",
+          'Élaborer et maintenir des tableaux comparatifs des ventes de marchandises par points de distribution.',
+        ],
+      },
+    ],
+
+    // Side Bar
+    contacts: {
+      title: 'Contacts',
+      phone: '+41 76 702 86 65',
+      email: 'daria.rodovnichenko@gmail.com',
+      linkedin: {
+        label: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/daria-rodovnichenko/',
+      },
+    },
+    educationTitle: 'Education',
+    education: [
+      {
+        school: 'Nomades Advanced Technologies | Genève',
+        specialization: 'Développeur Angular',
+        period: 'Mars 2025 - Mai 2025',
+      },
+      {
+        school: 'Nomades Advanced Technologies | Genève',
+        specialization: 'Développeur Java Script',
+        period: 'Janvier 2025 - Mars 2025',
+      },
+      {
+        school: 'IT School GoIt | Ukraine',
+        specialization: 'Programme Développeur Fullstack',
+        period: 'Février 2023 - Avril 2024',
+      },
+      {
+        university:
+          "Université Nationale de Commerce et d'Économie de Kyiv | Kyiv, Ukraine",
+        specialization: 'Master en Marketing',
+        period: 'Septembre 2010 - Septembre 2015',
+      },
+    ],
+    techSkills: [
+      {
+        title: 'Compétences Techniques',
+        skills: [
+          'HTML',
+          'CSS',
+          'JavaScript',
+          'React',
+          'TypeScript',
+          'Angular',
+          'Firebase',
+          'GIT',
+          'Slack',
+          'Trello',
+        ],
+      },
+    ],
+    softSkills: [
+      {
+        title: 'Compétences Personnelles',
+        skills: [
+          'Rigueur et souci du détail',
+          "Capacité d'organisation et de travail en autonomie",
+          'Excellentes aptitudes relationnelles et service client',
+        ],
+      },
+    ],
+    languages: [
+      {
+        title: 'Langues',
+        list: [
+          'Français: B2',
+          'Anglais: C1',
+          'Ukrainien: Langue maternelle',
+          'Russe',
+        ],
+      },
+    ],
+  },
+};
